2b95bb5a652d9301f245a19a56ae53948adae4a8
Noah A Klem <noahaklem@gmail.com>
2021-09-02 15:14:49 -0600

    commit tree 0353d261c5d458592c1fce1cf1feda6ee82686a5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+    
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        self.all.each do |i|
+            puts i.name
+        end
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+end
